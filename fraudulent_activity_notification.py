ef065c7da0135b8583d43e9d62a12536efefc932
Anton Tynnikov <tynnikov283@gmail.com>
2021-08-03 19:58:22 +0300

    add function for motify fraud activity

---
+++ fraudulent_activity_notification.py
@@ -0,0 +1,42 @@
+arr = [10, 20, 30, 40, 50]
+
+
+def calc_median(arr: list) -> int:
+    """
+
+    :param arr: list of trailing days
+    :return: median number
+    """
+
+    arr = sorted(arr)
+
+    if len(arr) == 0:
+        raise ValueError
+    middle = len(arr) // 2
+    if middle % 2 == 0:
+        return (arr[middle - 1] + arr[middle]) / 2
+    return arr[middle]
+
+
+def notify_fraudulent_activity(arr: list, d: int, n: int) -> int:
+    """
+    Function let to notify client when see fraud activity.
+
+    :param arr: list numbers
+    :param d: trailing days
+    :param n: number of days
+    :return: sum of notice
+    """
+    start = 0
+    end = d
+    notice = 0
+    while True:
+        if end == n:
+            break
+        new_arr = arr[start:end]
+        median = calc_median(new_arr) * 2
+        if arr[end] >= median:
+            notice += 1
+        start += 1
+        end += 1
+    return notice
